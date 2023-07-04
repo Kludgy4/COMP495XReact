569549d18ba0aa296ac1a11a86a3bc170276a39b
Kludgy4 <matt@brians.com.au>
2023-07-04 12:34:25 +1000

    Finalised Versioning

--- src/context/VersionContext.jsx
+++ src/context/VersionContext.jsx
@@ -2,16 +2,13 @@ import { DCTERMS, POSIX, RDF } from "@inrupt/vocab-common-rdf";
 import React, { createContext, useState } from "react";
 import { getInteger, getLinkedResourceUrlAll, getResourceInfo, getSolidDataset, getStringNoLocale, getThing, getUrl } from "@inrupt/solid-client";
 import { hasVersionPredicate, versionedInPredicate } from "../js/urls";
-import { displayError } from "../js/helper";
-import { getSpaceUntilMaxLength } from "@testing-library/user-event/dist/utils";
-import { useSession } from "@inrupt/solid-ui-react";
 
 export const VersionContext = createContext({
-  getVersionedDataset: (baseURL, version) => undefined,
-  getAvailableVersions: (baseURL) => undefined,
+  getVersionedDataset: (baseURL, version, options) => undefined,
+  getVersionedMetadata: (baseURL, options) => undefined,
 });
 
-export const VersionContextProvider = ({ children, initial = localStorage.getItem("podURL") }) => {
+export const VersionContextProvider = ({ children }) => {
 
   const getVersionedDataset = async (baseURL, version, options) => {
 
@@ -19,11 +16,10 @@ export const VersionContextProvider = ({ children, initial = localStorage.getIte
 
     // The resource requested is not available if
     if (
-      //   a. The version requested is outside of the available bounds
-      version < 1 ||
-      version > metadata[hasVersionPredicate] ||
-      //   b. A versioning predicate is not available and anything but version 1 is requested
-      (!metadata[hasVersionPredicate] || !metadata[versionedInPredicate]) && version !== 1
+      //   a. A versioning predicate is not available and anything but version 1 is requested
+      (!metadata[hasVersionPredicate] || !metadata[versionedInPredicate]) && version !== 1 ||
+      //   b. The version requested is outside of the available bounds
+      version < 1 || version > metadata[hasVersionPredicate]
     ) {
       throw new Error("Requested resource version does not exist");
     }
@@ -36,52 +32,35 @@ export const VersionContextProvider = ({ children, initial = localStorage.getIte
     return getSolidDataset(versionedURL, options);
   };
 
-  const getAvailableVersions = (baseURL) => {
-    return 1;
+  const getURLMetadata = async (url, options) => {
+    // Get the metadata
+    const baseDataset = await getResourceInfo(url, options);
+    const linkedResources = getLinkedResourceUrlAll(baseDataset);
+    const metadataURL = linkedResources["describedby"][0];
+
+    const metaset = await getSolidDataset(metadataURL, options);
+    const metathing = getThing(metaset, url);
+    if (metathing === null) {
+      throw new Error("No description resource metadata available");
+    }
+
+    // Extract the metadata into a convenient format
+    const pred = "http://www.w3.org/ns/ma-ont#format";
+    return {
+      [hasVersionPredicate]: getInteger(metathing, hasVersionPredicate),
+      [versionedInPredicate]: getUrl(metathing, versionedInPredicate),
+      [RDF.type]: getUrl(metathing, RDF.type),
+      [DCTERMS.modified]: getInteger(metathing, DCTERMS.modified),
+      [pred]: getStringNoLocale(metathing, pred),
+      [POSIX.mtime]: getInteger(metathing, POSIX.mtime),
+      [POSIX.size]: getInteger(metathing, POSIX.size)
+    };
   };
 
   return (
-    <VersionContext.Provider value={{ getVersionedDataset, getAvailableVersions }}>
+    <VersionContext.Provider value={{ getVersionedDataset, getURLMetadata }}>
       {children}
     </VersionContext.Provider>
   );
 };
 
-const getURLMetadata = async (url, options) => {
-  // Get the metadata
-  const baseDataset = await getResourceInfo(url, options);
-  const linkedResources = getLinkedResourceUrlAll(baseDataset);
-  const metadataURL = linkedResources["describedby"][0];
-
-  const metaset = await getSolidDataset(metadataURL, options);
-  const metathing = getThing(metaset, url);
-  if (metathing === null) {
-    throw new Error("No description resource metadata available");
-  }
-
-  // Extract the metadata into a convenient format
-  const metadata = {};
-  const hasVersion = getInteger(metathing, hasVersionPredicate);
-  if (hasVersion) metadata[hasVersionPredicate] = hasVersion;
-
-  const versionedIn = getUrl(metathing, versionedInPredicate);
-  if (versionedIn) metadata[versionedInPredicate] = versionedIn;
-
-  const type = getUrl(metathing, RDF.type);
-  if (type) metadata[RDF.type] = type;
-
-  const modified = getInteger(metathing, DCTERMS.modified);
-  if (modified) metadata[DCTERMS.modified] = modified;
-
-  const pred = "http://www.w3.org/ns/ma-ont#format";
-  const format = getStringNoLocale(metathing, pred);
-  if (format) metadata[pred] = format;
-
-  const mtime = getInteger(metathing, POSIX.mtime);
-  if (mtime) metadata[POSIX.mtime] = mtime;
-
-  const size = getInteger(metathing, POSIX.size);
-  if (size) metadata[POSIX.size] = size;
-
-  return metadata;
-};
